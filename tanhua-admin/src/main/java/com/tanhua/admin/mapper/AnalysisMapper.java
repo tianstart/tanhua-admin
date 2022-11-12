ad22e2b80539d8c7217102729f14ffccbb68f87a
Tian <2398026495@qq.com>
2022-11-12 21:11:30 +0800

    探花交友-v1.0.0

---
+++ tanhua-admin/src/main/java/com/tanhua/admin/mapper/AnalysisMapper.java
@@ -0,0 +1,8 @@
+package com.tanhua.admin.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.tanhua.model.domain.Analysis;
+
+
+public interface AnalysisMapper extends BaseMapper<Analysis> {
+}
